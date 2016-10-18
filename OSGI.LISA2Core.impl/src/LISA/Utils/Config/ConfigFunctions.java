5b402931efa980dd7c8c4d2d8d03bc5b7bbaec37
magnussonl <linusmagnusson@live.se>
2016-10-18 19:52:41 +0200

    read from file is fixed

--- OSGI.LISA2Core.impl/src/LISA/Utils/Config/ConfigFunctions.java
+++ OSGI.LISA2Core.impl/src/LISA/Utils/Config/ConfigFunctions.java
@@ -33,100 +33,90 @@ import org.osgi.framework.BundleContext;
  */
 public class ConfigFunctions {
 
-    private static Marshaller marshaller = null;
-    private static Unmarshaller unmarshaller = null;
-    private static JAXBContext jc = null;
-    private static String file = "application.conf";
+	private static Marshaller marshaller = null;
+	private static Unmarshaller unmarshaller = null;
+	private static JAXBContext jc = null;
+	private static String file = "application.conf";
 
-    //<editor-fold defaultstate="collapsed" desc="Getters and Setters">
-    private static void setJAXBContext() {
-        System.setProperty("javax.xml.bind.context.factory", "org.eclipse.persistence.jaxb.JAXBContextFactory");
-        
-            try {
-				jc =  AccessController.doPrivileged(new PrivilegedExceptionAction<JAXBContext>() {
+	// <editor-fold defaultstate="collapsed" desc="Getters and Setters">
+	private static void setJAXBContext() {
+		System.setProperty("javax.xml.bind.context.factory", "org.eclipse.persistence.jaxb.JAXBContextFactory");
 
-				    public JAXBContext run() throws JAXBException {
+		try {
+			jc = AccessController.doPrivileged(new PrivilegedExceptionAction<JAXBContext>() {
 
-				        // needs to run here otherwise throws AccessControlException
-				        return JAXBContext.newInstance(Config.class);
-				    }
-				});
-			} catch (PrivilegedActionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-    }
+				public JAXBContext run() throws JAXBException {
 
-    private static void setMarshaller() {
-        try {
-            marshaller = jc.createMarshaller();
-            marshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/json");
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        } catch (JAXBException ex) {
-            Logger.getLogger(ConfigFunctions.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+					// needs to run here otherwise throws AccessControlException
+					return JAXBContext.newInstance(Config.class);
+				}
+			});
+		} catch (PrivilegedActionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    private static void setUnmarshaller() {
-        try {
-            unmarshaller = jc.createUnmarshaller();
-            unmarshaller.setProperty(UnmarshallerProperties.MEDIA_TYPE, "application/json");
-            unmarshaller.setProperty(UnmarshallerProperties.JSON_INCLUDE_ROOT, true);
-        } catch (JAXBException ex) {
-            Logger.getLogger(ConfigFunctions.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+	private static void setMarshaller() {
+		try {
+			marshaller = jc.createMarshaller();
+			marshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/json");
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		} catch (JAXBException ex) {
+			Logger.getLogger(ConfigFunctions.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-    public static Config getConfig() {
-        Config retConf = new Config();
-       
-//            setJAXBContext();
-//            setUnmarshaller();
-//            
-//            String configIn = "{\"config\" : { \"ip\" : \"localhost\", \"username\" : \"karaf\", \"password\" : \"karaf\", \"port\" : 61616  }}";
-//            
-//            //URL configURL = context.getBundle().getEntry(file);
-//            //InputStream input = configURL.openStream();
-//            
-//            StreamSource json = new StreamSource(configIn);
-//            
-//            try {
-//				retConf = unmarshaller.unmarshal(json, Config.class).getValue();
-//			} catch (JAXBException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//            
-        retConf.setIp("localhost");
-        retConf.setUsername("karaf");
-        retConf.setPassword("karaf");
-        retConf.setPort(61616);
-            
-        
-        return retConf;
-    }
-    //</editor-fold>
+	private static void setUnmarshaller() {
+		try {
+			unmarshaller = jc.createUnmarshaller();
+			unmarshaller.setProperty(UnmarshallerProperties.MEDIA_TYPE, "application/json");
+			unmarshaller.setProperty(UnmarshallerProperties.JSON_INCLUDE_ROOT, true);
+		} catch (JAXBException ex) {
+			Logger.getLogger(ConfigFunctions.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-    public static void createConfigFile(String ip, Integer port) {
+	public static Config getConfig() {
+		Config retConf = new Config();
 
-        try {
-            setJAXBContext();
-            setMarshaller();
-            Config configToFile = new Config();
-            configToFile.setIp(ip);
-            configToFile.setPort(port);
+		setJAXBContext();
+		setUnmarshaller();
+		InputStream input = Config.class.getResourceAsStream(file);
 
-            FileWriter fw = new FileWriter(file);
-            StringWriter sw = new StringWriter();
+		StreamSource json = new StreamSource(input);
 
-            marshaller.marshal(configToFile, sw);
-            
-            fw.write(sw.toString());
-            fw.close();
-        } catch (JAXBException | IOException ex) {
-            Logger.getLogger(ConfigFunctions.class.getName()).log(Level.SEVERE, null, ex);
-        }
+		try {
+			retConf = unmarshaller.unmarshal(json, Config.class).getValue();
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-    }
+		return retConf;
+	}
+	// </editor-fold>
+
+	public static void createConfigFile(String ip, Integer port) {
+
+		try {
+			setJAXBContext();
+			setMarshaller();
+			Config configToFile = new Config();
+			configToFile.setIp(ip);
+			configToFile.setPort(port);
+
+			FileWriter fw = new FileWriter(file);
+			StringWriter sw = new StringWriter();
+
+			marshaller.marshal(configToFile, sw);
+
+			fw.write(sw.toString());
+			fw.close();
+		} catch (JAXBException | IOException ex) {
+			Logger.getLogger(ConfigFunctions.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+	}
 
 }
