24ebea678ef170fb0eeb3442ce752d6973bfc634
magnussonl <linusmagnusson@live.se>
2016-11-16 15:05:16 +0100

    deletion

--- OSGI.LISA.communication.service/src/OSGI/LISA/communication/HardwareCommunication.java
+++
@@ -1,9 +0,0 @@
-package OSGI.LISA.communication;
-
-import org.opcfoundation.ua.builtintypes.DataValue;
-
-public interface HardwareCommunication {
-	public boolean connect(String url);
-	public DataValue readValue(int lvl, String name);
-	public boolean setInt(int lvl, String name, int value);
-}
