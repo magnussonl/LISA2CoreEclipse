bb3d1e76a2d22866d30e2bfa2cc80c8ed7891d7d
magnussonl <linusmagnusson@live.se>
2017-02-14 23:04:14 +0100

    update

--- OSGi.LISA.sim.com.RobotACom2/src/osgi/lisa/sim/com/robotacom2/RobotAComOPC.java
+++ OSGi.LISA.sim.com.RobotACom2/src/osgi/lisa/sim/com/robotacom2/RobotAComOPC.java
@@ -2,28 +2,49 @@ package osgi.lisa.sim.com.robotacom2;
 
 
 
-import org.opcfoundation.ua.builtintypes.DataValue;
 
 import LISA.Utils.HardwareCommunication;
+import OPCAccess.OPCAccess;
 
 public class RobotAComOPC implements HardwareCommunication {
 
-	@Override
-	public boolean connect(String url) {
-		System.out.println("connected");
-		return false;
+	private OPCAccess opcAccess;
+	
+	public RobotAComOPC() {
+		opcAccess = new OPCAccess("localhost", "DESKTOP-OHVUEFM", "linusmagnusson@live.se", "Volvo-08", "137BB965-84BB-11D5-9FF1-00105A4AB1C6");
+		
 	}
 
 	@Override
-	public DataValue readValue(int lvl, String name) {
-		System.out.println("reading value in OPC");
-		return null;
+	public boolean connect(String url) {
+		
+		return false;
+	}
+	
+	
+
+	@Override
+	public String readValue(String itemId) {
+		return opcAccess.getItemValue(itemId);
 	}
 
 	@Override
 	public boolean setInt(int lvl, String name, int value) {
 		System.out.println("setting Int");
 		return false;
+	}
+
+	@Override
+	public void addItemToRead(String itemId) {
+		opcAccess.addItemAccessBase(itemId, 100);
+		opcAccess.startReading(itemId);
+		
+	}
+
+	@Override
+	public boolean checkValue(String itemId) {
+		// TODO Auto-generated method stub
+		return opcAccess.checkNewValue(itemId);
 	}
 	
 	
